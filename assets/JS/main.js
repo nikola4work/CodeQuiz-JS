04e2808e1dc887d7a51356f0dc9669a0957261eb
nikola <nikola4work@gmail.com>
2019-11-23 13:02:23 -0500

    questions

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -5,3 +5,29 @@ var op1 = document.getElementById("op1");
 var op2 = document.getElementById("op2");
 var op3 = document.getElementById("op3");
 var op4 = document.getElementById("op4");
+
+var app = {
+    question: [{
+            q: "Html stands for?",
+            options: [
+                "Hyper Text Markup Language",
+                "Hight Text Markup Language",
+                "Hyper Tabular Markup Language",
+                "None of the above"
+            ],
+            answer: 1
+        },
+
+        {
+            q: "Which of tag is used to mark begining of paragraph?",
+            options: ["TD", "br", "p", "tr"],
+            answer: 3
+        },
+
+        {
+            q: "Correct HTML tag for largest heading is?",
+            options: ["h4", "h1", "h8", "h9"],
+            answer: 2
+        }
+    ]
+};
