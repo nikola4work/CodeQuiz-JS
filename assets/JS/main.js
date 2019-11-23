425ece67beedbdeebb6e71312c0538a90a7c2ab4
nikola <nikola4work@gmail.com>
2019-11-23 12:52:56 -0500

    variables

---
+++ assets/JS/main.js
@@ -0,0 +1,7 @@
+var ul = document.getElementById("ul");
+var bvtn = document.getElementById("button");
+var scoreCard = Document.getElementById("questionBox");
+var op1 = document.getElementById("op1");
+var op2 = document.getElementById("op2");
+var op3 = document.getElementById("op3");
+var op4 = document.getElementById("op4");
